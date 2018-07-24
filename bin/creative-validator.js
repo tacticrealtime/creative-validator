c8791b7ad28ac4d51811278e690e699e32262c43
Maksim Rudoi <maksim.rudoi@tacticrealtime.com>
2018-07-24 14:54:39 +0300

    Formats Schema validation.

---
+++ bin/creative-validator.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+'use strict';
+
+const program = require('commander');
+const schemaValidator = require('../lib/formats-validator-v2.0');
+
+program
+	.version(require('../package.json').version)
+	.usage('[path]')
+	.description('Specify path to directory where creative is unzipped (manifest.json must be here). By default current path is used.')
+	.parse(process.argv);
+
+const path = program.args.shift() || '.';
+
+schemaValidator.validatePath(path);
