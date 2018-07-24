93e314f9f750ace8f03936b8c646be239bbafd48
Oleg Toming <oleg.toming@tacticrealtime.com>
2018-07-24 15:59:00 +0300

    Formatter for error messages

--- bin/creative-validator.js
+++ bin/creative-validator.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 'use strict';
 
-const program = require('commander');
+const program         = require('commander');
 const schemaValidator = require('../lib/formats-validator-v2.0');
+
+let errors = [];
+
+const format = function (allErrors) {
+	let messages = [];
+	allErrors.forEach(function (validatorErrors) {
+		validatorErrors.forEach(function (error) {
+			messages.push(JSON.stringify({message: error}));
+		});
+	});
+	console.log(messages.join());
+};
 
 program
 	.version(require('../package.json').version)
@@ -11,5 +23,15 @@ program
 	.parse(process.argv);
 
 const path = program.args.shift() || '.';
+let debug  = program.args.shift();
 
-schemaValidator.validatePath(path);
+if (debug === 'debug') {
+	debug = true;
+}
+else {
+	debug = false;
+}
+
+errors.push(schemaValidator.validatePath(path, debug));
+
+format(errors);
