324233e60d6d3cb3eeccc4cc12a1cbbc7446476f
Alex <a.moon@1sterling.com>
2023-06-15 10:53:18 +0300

    Updated deps

--- bin/creative-validator.js
+++ bin/creative-validator.js
@@ -1,57 +1,59 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-const program         = require('commander');
-const schemaValidator = require('../lib/formats-validator-v2.0');
-const securityValidator = require('../lib/security-validator-v2.0');
-const previewValidator = require('../lib/preview-validator-v2.0');
+const program = require("commander");
+const schemaValidator = require("../lib/formats-validator-v2.0");
+const securityValidator = require("../lib/security-validator-v2.0");
+const previewValidator = require("../lib/preview-validator-v2.0");
 
 let errors = [];
 
 const format = function (allErrors) {
-	let messages = [];
+  let messages = [];
 
-	try {
-		allErrors.forEach(function (validatorErrors) {
-			validatorErrors.forEach(function (error) {
-				messages.push(JSON.stringify({message: error}));
-			});
-		});
-	}
-	catch (e) {
-		messages.push(JSON.stringify({message: 'Validator error. Please contact support.'}));
-	}
-	console.log(messages.join());
+  try {
+    allErrors.forEach(function (validatorErrors) {
+      validatorErrors.forEach(function (error) {
+        messages.push(JSON.stringify({ message: error }));
+      });
+    });
+  } catch (e) {
+    messages.push(
+      JSON.stringify({ message: "Validator error. Please contact support." })
+    );
+  }
+  console.log(messages.join());
 };
 
 program
-	.version(require('../package.json').version)
-	.usage('[path]')
-	.description('Specify path to directory where creative is unzipped (manifest.json must be here). By default current path is used.')
-	.parse(process.argv);
+  .version(require("../package.json").version)
+  .usage("[path]")
+  .description(
+    "Specify path to directory where creative is unzipped (manifest.json must be here). By default current path is used."
+  )
+  .parse(process.argv);
 
-const path = program.args.shift() || '.';
-let debug  = program.args.shift();
+const path = program.args.shift() || ".";
+let debug = program.args.shift();
 
-if (debug === 'debug') {
-	debug = true;
-}
-else {
-	debug = false;
+if (debug === "debug") {
+  debug = true;
+} else {
+  debug = false;
 }
 
 errors.push(schemaValidator.validatePath(path, debug));
 errors.push(securityValidator.validatePath(path, debug));
 
-let isEmpty = a => Array.isArray(a) && a.every(isEmpty);
+let isEmpty = (a) => Array.isArray(a) && a.every(isEmpty);
 
 if (isEmpty(errors)) {
-	previewValidator.validatePath(path, program.previewPage, debug).then(function (e) {
-		errors.push(e);
-		format(errors);
-	});
+  previewValidator
+    .validatePath(path, program.previewPage, debug)
+    .then(function (e) {
+      errors.push(e);
+      format(errors);
+    });
+} else {
+  format(errors);
 }
-else{
-	format(errors);
-}
-
--- bin/security-validator.js
+++ bin/security-validator.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-const program = require('commander');
-const securityValidator = require('../lib/security-validator-v2.0');
+const program = require("commander");
+const securityValidator = require("../lib/security-validator-v2.0");
 
 program
-	.version(require('../package.json').version)
-	.usage('[path]')
-	.description('Specify path to directory where creative is unzipped (manifest.json must be here). By default current path is used.')
-	.parse(process.argv);
+  .version(require("../package.json").version)
+  .usage("[path]")
+  .description(
+    "Specify path to directory where creative is unzipped (manifest.json must be here). By default current path is used."
+  )
+  .parse(process.argv);
 
-const path = program.args.shift() || '.';
+const path = program.args.shift() || ".";
 
 securityValidator.validatePath(path, true);
